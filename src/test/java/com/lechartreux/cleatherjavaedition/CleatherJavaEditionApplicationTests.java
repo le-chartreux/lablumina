84792b094d8862fcb50b116c40f1276f822e63b8
le-chartreux <le-chartreux-vert@protonmail.com>
2023-11-08 10:44:35 +0100

    feat: create the project

---
+++ src/test/java/com/lechartreux/cleatherjavaedition/CleatherJavaEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lechartreux.cleatherjavaedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleatherJavaEditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
