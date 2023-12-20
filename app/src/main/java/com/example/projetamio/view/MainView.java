45b7242c657b4c9240163673580aaebdf297d978
le-chartreux <le-chartreux-vert@protonmail.com>
2023-12-20 16:28:57 +0100

    fix: clean the code

--- app/src/main/java/com/example/projetamio/view/MainView.java
+++ app/src/main/java/com/example/projetamio/view/MainView.java
@@ -24,15 +24,10 @@ public class MainView {
     private Button update;
     private Button configuration;
 
-    // Get singleton with all MainView information.
     public static MainView getInstance() {
         return MAIN_VIEW;
     }
 
-    /**
-     * Make the configuration of the Update button. This button will call a function to make a request
-     * and update the UI. Also, the button will be deactivated during a certain time to avoid spam.
-     */
     public void configureUpdateButton() {
         // When button is clicked, disable to button (to avoid spam) and make a request.
         update.setOnClickListener((view) -> {
@@ -46,11 +41,6 @@ public class MainView {
         });
     }
 
-    /**
-     * Update the UI according to data.
-     *
-     * @param fetchedData -> JSON returned from the request.
-     */
     public void updateViewAccordingToData(FetchedData fetchedData) {
         if (fetchedData.data.size() == 0) {
             Log.d("debug", "No data fetched");
@@ -62,38 +52,18 @@ public class MainView {
             SensorInformation currentSensorInformation = fetchedData.data.get(i);
             FetchedDataView currentFetchedDataView = fetchedDataViews.get(i);
 
-            // In case the sensor value is more than a specific threshold, led is on.
+            // When the sensor value is more than a specific threshold, led is on.
             if (currentSensorInformation.value > Constants.SENSOR_THRESHOLD)
-                currentFetchedDataView.getBulb().post(new Runnable() {
-                    @Override
-                    public void run() {
-                        currentFetchedDataView.getBulb().setImageResource(R.drawable.bulb_on);
-                    }
-                });
+                currentFetchedDataView.getBulb().post(() -> currentFetchedDataView.getBulb().setImageResource(R.drawable.bulb_on));
             else
-                currentFetchedDataView.getBulb().post(new Runnable() {
-                    @Override
-                    public void run() {
-                        currentFetchedDataView.getBulb().setImageResource(R.drawable.bulb_off);
-                    }
-                });
+                currentFetchedDataView.getBulb().post(() -> currentFetchedDataView.getBulb().setImageResource(R.drawable.bulb_off));
 
-            // Update the value of the sensor.
-            currentFetchedDataView.getValue().post(new Runnable() {
-                @Override
-                public void run() {
-                    currentFetchedDataView.getValue().setText("Mote: " + currentSensorInformation.mote + " - Value : " + currentSensorInformation.value);
-                }
-            });
+            currentFetchedDataView.getValue().post(() -> currentFetchedDataView.getValue().setText("Mote: " + currentSensorInformation.mote + " - Value : " + currentSensorInformation.value));
         }
 
-        time.post(new Runnable() {
-            @Override
-            public void run() {
-                // Format the date to a more readable value, and then update the UI with the current time.
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-                time.setText(formatter.format(LocalDateTime.now()));
-            }
+        time.post(() -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            time.setText(formatter.format(LocalDateTime.now()));
         });
     }
 
