f947ad314d0abc07fa355d7c4ffde37d92b6d8c9
Frere Swan <frere14u@telecomnancy.eu>
2023-12-20 16:09:36 +0100

    fix: SensorInformation.java

--- app/src/main/java/com/example/projetamio/fetchedData/SensorInformation.java
+++ app/src/main/java/com/example/projetamio/fetchedData/SensorInformation.java
@@ -3,4 +3,7 @@ package com.example.projetamio.fetchedData;
 public class SensorInformation {
     public float value;
     public float mote;
+    //Only used for deserialization
+    public String timestamp;
+    public String label;
 }
